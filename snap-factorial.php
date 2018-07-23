c1511eee93bc18a3316fe1dd694d02070623cec6
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-23 09:06:39 -0600

    Fixed typo

--- snap-factorial.php
+++ snap-factorial.php
@@ -1,14 +1,14 @@
 <?php
 
-function calculate(int $n) : int {
+function calculate($n) {
 	$x = 1;
 	if($n < 0 || $n > 20) {
 		throw (new \RangeException("Cannot be a negative number or be greater than 20"));
 	}
-	for($i=1;$i<=$n-1;$i++) {
-		$x*= ($i+1);
+	for($i=1 ;$i<=$n; $i++) {
+		$x*= $i;
 	}
 	return("The factorial of $n = $x"."\n");
 }
 
-echo calculate(8);
+echo calculate(5);
