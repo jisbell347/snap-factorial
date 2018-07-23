e421cfcccb9fe62928c056e8119bac23a3fc1be2
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-23 08:47:55 -0600

    added parameter and arguement to function

--- snap-factorial.php
+++ snap-factorial.php
@@ -1,18 +1,14 @@
 <?php
 
-function calculate() {
-	$n = 6;
+function calculate($n) {
 	$x = 1;
 	for($i=1;$i<=$n-1;$i++) {
 		$x*= ($i+1);
-		if($n < 0) {
-			throw (new \InvalidArgumentException("Cannot be a negative number"));
-		}
-		if($n > 20) {
-			throw (new \RangeException("Initial number cannot exceed 20"));
+		if($n < 0 || $n > 20) {
+			throw (new \RangeException("Cannot be a negative number or be greater than 20"));
 		}
 	}
 	return("The factorial of $n = $x"."\n");
 }
 
-echo calculate();
+echo calculate(8);
