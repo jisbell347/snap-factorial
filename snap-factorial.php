38d33a0dd34a1ba382f39cfb8cce12095c157644
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-23 08:41:30 -0600

    added if statement with \RangeException error

--- snap-factorial.php
+++ snap-factorial.php
@@ -5,8 +5,11 @@ function calculate() {
 	$x = 1;
 	for($i=1;$i<=$n-1;$i++) {
 		$x*= ($i+1);
-		if($n < 0 || $n > 20) {
-			throw (new \RangeException("Cannot be a negative number"));
+		if($n < 0) {
+			throw (new \InvalidArgumentException("Cannot be a negative number"));
+		}
+		if($n > 20) {
+			throw (new \RangeException("Initial number cannot exceed 20"));
 		}
 	}
 	return("The factorial of $n = $x"."\n");
