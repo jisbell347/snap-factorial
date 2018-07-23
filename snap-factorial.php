dc8490b366d61545955c5c95138e0b56c10ea422
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-23 08:59:12 -0600

    moved if statement out of for loop

--- snap-factorial.php
+++ snap-factorial.php
@@ -1,12 +1,12 @@
 <?php
 
-function calculate($n) {
+function calculate(int $n) : int {
 	$x = 1;
+	if($n < 0 || $n > 20) {
+		throw (new \RangeException("Cannot be a negative number or be greater than 20"));
+	}
 	for($i=1;$i<=$n-1;$i++) {
 		$x*= ($i+1);
-		if($n < 0 || $n > 20) {
-			throw (new \RangeException("Cannot be a negative number or be greater than 20"));
-		}
 	}
 	return("The factorial of $n = $x"."\n");
 }
